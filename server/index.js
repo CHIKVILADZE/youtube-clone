6e30f4e74755ca0519d2f3cc129364747b676412
unknown <chikviladze555@yahoo.com>
2024-05-09 18:04:13 +0400

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from  'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 
 const app = express();
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connect to Database")
+    }).catch((err) => {
+        throw err
+    })
+}
 
 
 app.listen(8800, () => {
+    connect()
     console.log("App listen to port 8800")
 })
