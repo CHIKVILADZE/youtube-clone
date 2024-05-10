e52df6fa1be440eee48af5e56dd1a717246065dc
unknown <chikviladze555@yahoo.com>
2024-05-10 18:00:51 +0400

    add routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from  'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoute from './routes/users.js'
+import videoRoute from './routes/videos.js'
+import commentRoute from './routes/comments.js'
+import authRoute from './routes/auth.js'
+
 
 
 const app = express();
@@ -13,6 +18,24 @@ const connect = () => {
         throw err
     })
 }
+
+app.use(express.json())
+app.use("/api/users", userRoute)
+app.use("/api/comments", commentRoute)
+app.use("/api/videos", videoRoute)
+app.use("/api/auth", authRoute)
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong"
+    return res.status(status).json({
+        success:false,
+        status:status,
+        message:message
+    })
+})
+
+
 
 
 app.listen(8800, () => {
