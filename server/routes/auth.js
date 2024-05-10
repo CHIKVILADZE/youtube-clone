b9f643318e97a7b7206cdb1f4f16ef4b8000b9f6
unknown <chikviladze555@yahoo.com>
2024-05-10 18:57:14 +0400

    add signin controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup } from '../controllers/auth.js';
+import { signin, signup } from '../controllers/auth.js';
 
 const router = express.Router();
 
@@ -11,7 +11,7 @@ router.post("/signup", signup)
 
 // Sign In
 
-router.post("/signin")
+router.post("/signin", signin)
 
 
 
