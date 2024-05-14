332417a2310637fabbb0bdf06da2ee3485eb64bf
unknown <chikviladze555@yahoo.com>
2024-05-14 16:28:26 +0400

    fix position:stycky problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,22 @@ import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: "Radio Canada Big", sans-serif;
+  }
+`;
+
 const Container = styled.div`
   display: flex;
   margin: 0;
-  overflow: hidden;
-  border: 4px solid red;
 `;
 
 const Main = styled.div`
   flex: 7;
+  background-color: #181818;
 `;
 
 const Wrapper = styled.div``;
@@ -21,10 +28,31 @@ const Wrapper = styled.div``;
 function App() {
   return (
     <Container>
+      <GlobalStyle />
+
       <Menu />
       <Main>
         <Navbar />
-        <Wrapper>Video Cards</Wrapper>
+        <Wrapper>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1>
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>{" "}
+          <h1>Hello</h1> <h1>Hello</h1> <h1>Hello</h1>
+          <h1>Hello</h1>
+        </Wrapper>
       </Main>
     </Container>
   );
