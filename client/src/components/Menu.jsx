576d5fc7d17f34a77cf64aacf21b38919c23be97
unknown <chikviladze555@yahoo.com>
2024-05-15 19:22:54 +0400

    add hover in menu item

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -52,6 +52,10 @@ const Item = styled.div`
   gap: 20px;
   cursor: pointer;
   padding: 2px 0px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.soft};
+  }
 `;
 
 const Hr = styled.hr`
