7adf73183e9139d4392585f954025ef00e11e266
unknown <chikviladze555@yahoo.com>
2024-05-22 15:57:44 +0400

    upload files usinf firebase

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const Container = styled.div`
   width: 100%;
@@ -64,18 +70,97 @@ const Label = styled.label`
 `;
 
 export default function Upload({ setOpen }) {
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+  const [imgPerc, setImgPerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [inputs, setInputs] = useState({});
+  const [tags, setTags] = useState([]);
+
+  const handleTags = (e) => {
+    setTags(e.target.value.split(","));
+  };
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage();
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl" ? setImgPerc(progress) : setVideoPerc(progress);
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    uploadFile(vide);
+  }, [video]);
+  useEffect(() => {
+    uploadFile(img);
+  }, [img]);
+
   return (
     <Container>
       <Wrapper>
         <Close onClick={() => setOpen(false)}>X</Close>
         <Title>Upload a New Video</Title>
         <Label>Video:</Label>
-        <Input type="file" accept="/video/*" />
-        <Input type="text" placeholder="Title" />
-        <Desc placeholder="Description" rows={8} />
-        <Input type="text" placeholder="Separate the tags with commas." />
+        <Input
+          type="file"
+          accept="/video/*"
+          onChange={(e) => setVideo(e.target.files[0])}
+        />
+        <Input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <Desc placeholder="Description" rows={8} onChange={handleChange} />
+        <Input
+          type="text"
+          name="desc"
+          placeholder="Separate the tags with commas."
+          onChange={handleTags}
+        />
         <Label>Image:</Label>
-        <Input type="file" accept="/image/*" />
+        <Input
+          type="file"
+          accept="/image/*"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
         <Button>Upload</Button>
       </Wrapper>
     </Container>
