ba6c384c0bf1e7fd04b11d6fa244646cd89b9333
unknown <chikviladze555@yahoo.com>
2024-05-25 14:01:58 +0400

    change render baseURL

--- client/src/utils/API.jsx
+++ client/src/utils/API.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://youtube-clone-1i99.onrender.com/api",
   withCredentials: true,
 });
 
